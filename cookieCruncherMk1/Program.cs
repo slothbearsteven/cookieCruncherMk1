6f5b1707895758bedda9caf7fb0f73b032afbe69
Steven Kennedy <Kennedy.Steven.P@gmail.com>
2022-08-13 11:43:26 -0600

    Add project files.

---
+++ cookieCruncherMk1/Program.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using NUnit.Framework;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+
+namespace cookieCruncherMk1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://orteil.dashnet.org/cookieclicker/test/";
+
+            driver.Quit();
+
+        }
+
+        void CookieClicker(IWebDriver driver)
+        {
+
+
+        }
+     
+    }
+}
